1e9f2eebb17cd5b792707c8adfedeb7718474dcd
Francisco Medina <franciscomedinaag@gmail.com>
2020-01-29 15:43:26 -0600

    no se ayuda

---
+++ server/create-lb-tables.js
@@ -0,0 +1,9 @@
+var server = require('./server');
+var ds = server.dataSources.db;
+var lbTables = ['product'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
+
