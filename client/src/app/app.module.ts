0fd4268b7ef6f49abe7eeda005b4f80ff4fe7227
Francisco Medina <franciscomedinaag@gmail.com>
2020-02-01 18:47:35 -0600

    crud en consola y auth service
    falta hacer el login, crud de usuarios y acl al modelo

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+
 //CRM
 import { LoginComponent } from './components-crm/login/login.component';
 import { ClientesComponent } from './components-crm/clientes/clientes.component';
@@ -14,11 +16,13 @@ import { ContactoComponent } from './components-web/contacto/contacto.component'
 import { MueblesComponent } from './components-web/muebles/muebles.component';
 import { NosotrosComponent } from './components-web/nosotros/nosotros.component';
 
-//servicios navbar y footer
+//servicios 
 import { NavbarCrmComponent } from './services/crm/navbar-crm/navbar-crm.component';
 import { FooterCrmComponent } from './services/crm/footer-crm/footer-crm.component';
 import { NavbarWebsiteComponent } from './services/website/navbar-website/navbar-website.component';
 import { FooterWebsiteComponent } from './services/website/footer-website/footer-website.component';
+import { DataApiService } from './services/data-api.service';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -36,9 +40,10 @@ import { FooterWebsiteComponent } from './services/website/footer-website/footer
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataApiService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/components-crm/clientes/clientes.component.ts
+++ client/src/app/components-crm/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from '../../services/data-api.service';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api:DataApiService) { }
 
   ngOnInit() {
+    this.getClients(); 
+  }
+
+  getClients(){
+    this.api.getAllClients()
+    .subscribe((clients)=>console.log("productos: ", clients));
   }
 
 }
