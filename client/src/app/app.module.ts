bd099eea04d736495a407bba86ce02c06ec57c29
Francisco Medina <franciscomedinaag@gmail.com>
2020-02-19 00:07:44 -0600

    RQF9/RQF11 add and read clients with filter
    sigue mandar correos :)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule }   from '@angular/forms';
 import {ToastrModule} from 'ngx-toastr';
+import {NgSelectModule} from '@ng-select/ng-select'
 
 //CRM
 import { LoginComponent } from './components-crm/login/login.component';
@@ -21,6 +22,11 @@ import { NosotrosComponent } from './components-web/nosotros/nosotros.component'
 import { InformeVenComponent } from './components-crm/informe-ven/informe-ven.component';
 import { MiperfilComponent } from './components-crm/miperfil/miperfil.component';
 import { GraficasComponent } from './components-crm/graficas/graficas.component';
+import { UsuariosComponent } from './components-crm/usuarios/usuarios.component';
+import { ToastService } from './services/toast.service';
+import { FileChooserComponent } from './components-crm/file-chooser/file-chooser.component';
+import { ProfileImageEditorComponent } from './components-crm/profile-image-editor/profile-image-editor.component';
+import { FichaClienteComponent } from './components-crm/ficha-cliente/ficha-cliente.component';
 
 //servicios 
 import { NavbarCrmComponent } from './services/crm/navbar-crm/navbar-crm.component';
@@ -29,10 +35,6 @@ import { NavbarWebsiteComponent } from './services/website/navbar-website/navbar
 import { FooterWebsiteComponent } from './services/website/footer-website/footer-website.component';
 import { DataApiService } from './services/data-api.service';
 import { AuthService } from './services/auth.service';
-import { UsuariosComponent } from './components-crm/usuarios/usuarios.component';
-import { ToastService } from './services/toast.service';
-import { FileChooserComponent } from './components-crm/file-chooser/file-chooser.component';
-import { ProfileImageEditorComponent } from './components-crm/profile-image-editor/profile-image-editor.component';
 
 @NgModule({
   declarations: [
@@ -52,12 +54,14 @@ import { ProfileImageEditorComponent } from './components-crm/profile-image-edit
     MiperfilComponent,
     GraficasComponent,
     FileChooserComponent,
-    ProfileImageEditorComponent
+    ProfileImageEditorComponent,
+    FichaClienteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgSelectModule,
     BrowserAnimationsModule,  
     FormsModule,
     ToastrModule.forRoot()
--- client/src/app/components-crm/clientes/clientes.component.ts
+++ client/src/app/components-crm/clientes/clientes.component.ts
@@ -1,31 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { DataApiService } from '../../services/data-api.service';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { ToastService } from '../../services/toast.service';
 
-
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  styleUrls: ['./clientes.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ClientesComponent implements OnInit {
 
-  //private user:any={};
+  private fecha:any;
+  private client:any={};
+  private clients:any=[];
+  private filtered:any=[];
+  private estados:any=["Jalisco","CDMX","Cd. Juárez","Nuevo León","Morelia","Veracruz"];
+  private word:string;
 
   constructor(private api:DataApiService, 
     private auth:AuthService,
     private router:Router,
-    private toastr:ToastService) { }
+    private toast:ToastService) { }
 
   ngOnInit() {
-    // this.user=this.auth.getCurrentUser();
-    // if(this.user.active==false){
-    //   this.toastr.showError('Su cuenta se encuentra inactiva');
-    //   this.auth.logoutUser();
-    //   this.router.navigate(['/login']);
-    //}
+   this.getClients();
+  }
+
+  saveClient(){
+    console.log("client: ", this.client)
+      this.fecha=new Date().toISOString();
+      this.client.registro=this.fecha;
+      this.client.cantTratos=0;
+      this.client.real=false;
+
+      this.api.post('/Clients',this.client)
+      .subscribe((done)=>{
+        this.toast.showSuccess("Cliente registrado")
+        this.client={}
+      //  this.getClients();
+      },
+      (err) => {
+        this.toast.showError("Los datos introducidos son incorrectos")
+      });
+  }
+
+  getClients(){
+    this.api.get('/Clients',true)
+      .subscribe((clients)=>{
+        this.clients=clients;
+        this.filtered=this.clients;
+      })
+  }
+
+  onSearchChange(searchValue: string): void {  
+    this.filtered=[];
+    this.clients.forEach(client => {
+      // console.log(client.nombre)
+      // console.log(client.nombre.indexOf(searchValue));
+      if(client.nombre.toLowerCase().indexOf(searchValue.toLowerCase())!=-1){
+        this.filtered.push(client);
+      }
+
+    });
   }
 
 }
