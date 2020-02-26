6656b25285a5f9cc453894270dc120bce1fd860b
Francisco Medina <franciscomedinaag@gmail.com>
2020-02-25 21:34:50 -0600

    catalogo delete & create (falta mandarlos por email)
    este proyecto esta de la verga aaaaah

--- client/src/app/components-crm/catalogos/catalogos.component.ts
+++ client/src/app/components-crm/catalogos/catalogos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from '../../services/data-api.service';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-catalogos',
@@ -7,9 +9,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogosComponent implements OnInit {
 
-  constructor() { }
+  private catalogue:string=" "
+  private file:string=" "
+  private ext:string=" "
+  private send:boolean=true
+  private newFile:any={}
+  private catalogos:any;
+
+  constructor(private api:DataApiService,private toast:ToastService) { }
 
   ngOnInit() {
+    this.getCat();
   }
+
+  getCat(){
+    this.api.get('/Catalogos')
+    .subscribe((catalogos)=>{
+      this.catalogos=catalogos;
+    })
+  }
+
+  selectImageOrder( file:any){
+    this.file=file.base64File;
+    this.ext=file.fileExtention;
+    this.newFile=file;
+  }
+
+  upload(){
+    if(this.ext=='.pdf'){
+    if(this.file.length>5 && this.catalogue.length>1){
+       let data:any={
+         newFile:this.newFile,
+         nombre:this.catalogue
+       }
+        this.api.post('/Catalogos/setFile',data)
+        .subscribe((uploaded)=>{
+          this.toast.showSuccess("Catalogo subido")
+          this.getCat();
+        },err=>{
+          this.toast.showError("¡Documento muy grande!")
+        })
+      }
+      else{
+        this.toast.showError("No se han llenado todos los campos");
+      } 
+    }
+    else{
+      this.toast.showError("Debe ser PDF")
+    }
+  }
+
+
+  deleteCat(fileId,id){
+    
+    let data:any={
+      id:fileId
+    }
+    if(confirm("¿Desea eliminar la nota?")){
+      this.api.post('/Catalogos/deleteFile',data)
+      .subscribe((okay)=>{
+        this.api.delete(`/Catalogos/${id}`)
+        .subscribe((okay)=>{
+          this.getCat();
+          this.toast.showSuccess("Eliminado con exito")
+        })
+      })
+    }
+  }
+
+
+  showPdf(id) {
+    let name;
+    let base64;
+    this.catalogos.forEach(cat => {
+      if(cat.id==id){
+        name=cat.nombre;
+        base64=cat.file;
+      }
+    });
+
+    const linkSource = 'data:application/pdf;base64,'+base64;
+    const downloadLink = document.createElement("a");
+    const fileName = name+'.pdf';
+
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    downloadLink.click();
+  }
+
 
 }
