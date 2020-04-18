a1723acd239c917187d82d641931990ca6244af1
Francisco Medina <franciscomedinaag@gmail.com>
2020-04-18 13:36:54 -0500

    procfile

--- server/boot/create-lb-tables.js
+++ server/boot/create-lb-tables.js
@@ -3,6 +3,7 @@ var ds = server.dataSources.db;
 var lbTables = [];
 ds.autoupdate( function(er) {
   if (er) throw er;
+  
   console.log('Loopback ALL tables created in ', ds.adapter.name);
   ds.disconnect();
 });
